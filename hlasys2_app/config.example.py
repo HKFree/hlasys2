530679bc70cb2474671be690a0f6e67d951def76
MrReactive <ondrap266@gmail.com>
2026-03-04 19:39:08 +0100

    update config example

--- hlasys2_app/config.example.py
+++ hlasys2_app/config.example.py
@@ -1,21 +1,27 @@
-FLASK_SECRET_KEY = "<secret>"
-USERDB_API_USER = "<api-user>"
-USERDB_API_KEY = "<api-key>"
-USERDB_API_CACHE_TIMEOUT_HOURS = 1
-USERDB_API_URL = "<hkf-userdb-url>"
-APP_BASE_URL = "https://hlasovani-dev.hkfree.org"
-SLACK_WEBHOOK_URL_VV = "<wenhook-url>"
-SLACK_WEBHOOK_URL_PD = "<wenhook-url>"
-SLACK_WEBHOOK_URL_CS = "<wenhook-url>"
+from os import environ
 
+FLASK_SECRET_KEY = ""
+USERDB_API_USER = ""
+USERDB_API_KEY = ""
+USERDB_API_CACHE_TIMEOUT_HOURS = 1
+USERDB_API_URL = "https://userdb.hkfree.org/userdb/api/hlasys/get-spravce?typSpravce="
+APP_BASE_URL = ""
+SLACK_WEBHOOK_URL = ""
+SLACK_WEBHOOK_URL_VV = SLACK_WEBHOOK_URL
+SLACK_WEBHOOK_URL_PD = SLACK_WEBHOOK_URL
+SLACK_WEBHOOK_URL_CS = SLACK_WEBHOOK_URL
 # Users that can change state of a proposal
 USERS_CHANGE_STATE = [656, 9000]
 
-DEBUG = False
+DEBUG = True
 
 class HlasysConfig:
     OIDC_SCOPES = "phone openid email groupshkfree"
     OIDC_CLIENT_SECRETS = "client_secrets.json"
-    OIDC_SERVER_METADATA_URL = "<oidc-metadata-url>"
+    OIDC_SERVER_METADATA_URL = "https://sso.hkfree.org/realms/hkftests/.well-known/openid-configuration"
     OIDC_OVERWRITE_REDIRECT_URI = "https://hlasovani-dev.hkfree.org/oidc_callback"
-    APP_VERSION = "2.0.4-dev"
+    
+    HLASYS2_VERSION = "2.0.4-dev"
+    HLASYS2_REF_NAME = environ.get('HLASYS2_REF_NAME', 'unknown_ref')
+    HLASYS2_COMMIT_HASH = environ.get('HLASYS2_COMMIT_HASH', 'unknown_hash')
+    
